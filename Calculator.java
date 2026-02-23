9f0f2ded3847856ead35e560f14fb0c4d0f7f0d8
Shubham Suryawanshi <Shubhambs1106@gmail.com>
2026-02-24 00:23:16 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,63 +1,30 @@
-import java.util.Scanner;
-
 public class Calculator {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("=== Java Calculator ===");
-
-        // First number
-        System.out.print("Enter first number: ");
-        if (!sc.hasNextDouble()) {
-            System.out.println("Invalid first number!");
-            return;
-        }
-        double num1 = sc.nextDouble();
-
-        // Operator
-        System.out.print("Enter operator (+, -, *, /): ");
-        char operator = sc.next().charAt(0);
-
-        // Second number
-        System.out.print("Enter second number: ");
-        if (!sc.hasNextDouble()) {
-            System.out.println("Invalid second number!");
-            return;
-        }
-        double num2 = sc.nextDouble();
+        double num1 = 15;
+        double num2 = 5;
+        char operator = '+';
 
         double result;
 
-        switch (operator) {
-
-            case '+':
-                result = num1 + num2;
-                break;
-
-            case '-':
-                result = num1 - num2;
-                break;
-
-            case '*':
-                result = num1 * num2;
-                break;
-
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("Error: Division by zero");
-                    return;
-                }
-                result = num1 / num2;
-                break;
-
-            default:
-                System.out.println("Invalid operator");
-                return;
+        if (operator == '+') {
+            result = num1 + num2;
+        } 
+        else if (operator == '-') {
+            result = num1 - num2;
+        } 
+        else if (operator == '*') {
+            result = num1 * num2;
+        } 
+        else if (operator == '/') {
+            result = num1 / num2;
+        } 
+        else {
+            System.out.println("Invalid operator");
+            return;
         }
 
-        System.out.println("Result: " + result);
-        sc.close();
+        System.out.println("Result = " + result);
     }
 }
